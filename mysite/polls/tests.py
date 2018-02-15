376a67617df42a248218c8d5d42f8d5d1eb80ddd
lyf118811 <lyf118811@gmail.com>
2018-02-14 22:17:02 -0500

    2018/2/14

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,17 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() 应该对 pub_date 字段值是将来的那些问题返回 False。
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
